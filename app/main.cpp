e33b8981a61ccbdf408b0657b5063e6cbd525ab6
foxbandykoot <lerenard.charly@gmail.com>
2025-10-20 19:09:24 +0200

    add option in main CMakeLists, clean other CMakeLists, add hpp to send data from CMakeLists to cpp.

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,13 @@
 #include "../src/lib_kelvin/kelvin.h"
 #include "../src/lib_fahrenheit/fahrenheit.h"
+#include "version.hpp"
 
 int main(void)
 {
-    
-    float celsius = 25.0f;
+    std::cout << "Temperature Converter Application - Name Project "<< cProjectName << std::endl;
+    std::cout << "Temperature Converter Application - Version: " << cProjectVersion << std::endl;
+
+    float celsius = 35.0f;
     float kelvin = celsiusToKelvin(celsius);
     printKelvinToCelsius(kelvin, celsius);
 
